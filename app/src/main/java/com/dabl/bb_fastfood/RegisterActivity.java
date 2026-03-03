1c2bc03b7c8ecd10954b9b15173de6d06d51456b
Diego Barragan <dabarraganlo@gmail.com>
2026-03-03 10:39:33 -0500

    Crear Activity de registro

--- app/src/main/java/com/dabl/bb_fastfood/RegisterActivity.java
+++ app/src/main/java/com/dabl/bb_fastfood/RegisterActivity.java
@@ -1,24 +1,37 @@
 package com.dabl.bb_fastfood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnCrearCuenta, btnVolverLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnCrearCuenta = findViewById(R.id.btnCrearCuenta);
+        btnVolverLogin = findViewById(R.id.btnVolverLogin);
+
+        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, HomeMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnVolverLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
